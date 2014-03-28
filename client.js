6959cfa3e862a14145f52458654d891c81353f4f
michellejzh <michelle_zheng@brown.edu>
2014-03-28 11:51:26 -0400

    added more notes

--- client.js
+++ client.js
@@ -1,14 +1,52 @@
+/*
+Functions in this file:
+	initializeMap - creates the map on page load
+	placeMarker - places a marker on the map corresponding to a vendor
+	centerMarker - center a certain marker, called when you click on a vendor's info
+
+	Some to do:
+		should we have a seperate file for map JS and field/search JS?
+
+		everything related to search
+			pass search results to the DOM for display
+			
+*/
+
 //initiate the map
 function initializeMap() {
 	var mapOptions = {
 		center: new google.maps.LatLng(40.7078, -74.0119),
-		zoom: 15
+		zoom: 16
 	};
 	var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 	console.log("there should be a new map now");
 }
 
 google.maps.event.addDomListener(window, 'load', initializeMap);
+
+//input: results of a search
+//output: places markers on the map corresponding to the coordinates of each result
+function placeMarker(searchResults) {
+	for (i=0; i=len(searchResults; i++)) {
+		var point = convertToCoord(searchResults[i].address);
+		_map.setCenter(point);
+		var marker = new google.maps.Marker({
+			'map' : _map,
+			'position' : point
+		});
+	}
+}
+
+//convert address to coordinates
+function convertToCoord(address) {
+	//		#TO DO
+}
+
+function centerMarker() {
+	//navigate so that the selected business (from sidebar)
+	//is centered on the map
+
+}
 
 function test() {
 	alert("it works!");
---
+++ server.js
@@ -0,0 +1,24 @@
+/*
+
+Table parameters:
+	name
+	address
+	productCapabilityIds
+		0 = plants
+		1 = flowers
+		2 = orchid
+	paymentTerms
+		1 = none specified
+		2 = CC preferred
+		3 = CC required
+		4 = invoice
+	leadTime
+		1 = none specified
+		2 = same day
+		3 = next day
+		4 = 2+ days
+	rates
+		% in decimal format
+	delivery
+		in dollar$
+		
