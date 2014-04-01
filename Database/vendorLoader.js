a888ae6702f5b9e2c3492a716a0b54a067edb26a
fdhoff <foster_hoff@brown.edu>
2014-04-01 11:04:59 -0400

    Running "node vendorLoader.js" will create our database

---
+++ Database/vendorLoader.js
@@ -0,0 +1,15 @@
+var anyDB = require('any-db');
+var zipcoder = require('./vendorer.js');
+
+var conn = anyDB.createConnection('sqlite3://vendors.db');
+
+conn.query('CREATE TABLE vendors (primaryKey INTEGER, name TEXT, address TEXT, city TEXT, state TEXT, zipcode TEXT, phone TEXT, email TEXT, website TEXT, productCapabilityId INTEGER, paymentTerms INTEGER, leadTime INTEGER, rate DOUBLE, deliveryFee DOUBLE)')
+    .on('end', function() {
+      console.log('Made table!');
+    });
+
+zipcoder('some_zipcodes.csv', function(region) {
+  conn.query('INSERT INTO vendors VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)', [region.primaryKey, region.names, region.address, region.city, region.state, region.zipcode, region.phone, region.email, region.website, region.productCapabilityId, region.paymentTerms, region.leadTime, region.rate, region.deliveryFee])
+    .on('error', console.error);
+});
+
