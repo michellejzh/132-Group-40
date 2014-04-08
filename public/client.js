11ccb1e17ab28d22c83eb52041a377fd2e3efd10
michellejzh <michelle_zheng@brown.edu>
2014-04-08 16:37:42 -0400

    fiwrote distance function

--- public/client.js
+++ public/client.js
@@ -2,13 +2,50 @@
 function initializeMap() {
 	var mapOptions = {
 		center: new google.maps.LatLng(40.7078, -74.0119),
-		zoom: 16
+		zoom: 15
 	};
 	map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 	geocoder = new google.maps.Geocoder();
+	service = new google.maps.DistanceMatrixService();
 }
 
 google.maps.event.addDomListener(window, 'load', initializeMap);
+
+
+
+function calcDistance(clientAddress, vendorAddress) {
+  service.getDistanceMatrix(
+    {
+      origins: [clientAddress, vendorAddress],
+      destinations: [clientAddress, vendorAddress],
+      travelMode: google.maps.TravelMode.DRIVING,
+      unitSystem: google.maps.UnitSystem.IMPERIAL,
+      avoidHighways: false,
+      avoidTolls: false
+    }, callback);
+}
+
+function callback(response, status) {
+	if (status != google.maps.DistanceMatrixStatus.OK) {
+    	alert('Error was: ' + status);
+	} 
+	else {
+		var origins = response.originAddresses;
+    	var destinations = response.destinationAddresses;
+    	var totalDist = 0;
+		for (var i = 0; i < origins.length; i++) {
+    		var results = response.rows[i].elements;
+    		addMarker(origins[i], false);
+	      	for (var j = 0; j < results.length; j++) {
+				totalDist = totalDist + results[j].distance.value;
+	      	}
+	    }
+	    //convert to miles
+	    totalDist = 0.000621371*totalDist
+	    alert(totalDist+" miles");
+	}
+}
+
 
 function moveMapCenter() {
 	console.log(document.vendorAddress);
@@ -24,9 +61,11 @@ function moveMapCenter() {
 
 //needs to be broken into helper methods
 function returnResults() {
-	console.log('about to find JSON');
+	addResultsToList();
+
 	//get the results to return
 	//THIS MEANS: REPLACE SEARCH.JSON WITH EACH SEARCH
+		//do we need search IDs?
 	var request = new XMLHttpRequest();
     request.open('GET', '/search.json', true);
     request.addEventListener('load', function(e){
@@ -35,22 +74,30 @@ function returnResults() {
             var content = request.responseText;
             var data = JSON.parse(content);
             console.log(data);
+            addResultsToList(data);
         }
     }, false);
-	
-    //char to add: name, address, short desc, 
-    var currAddress = "256 Thayer St Providence RI 02906";
+}
 
-    //add them to the DOM
-	var ul = document.getElementById("results-list");
-	var li = document.createElement('li');
-	var id = Math.random(150000); //id of the client
-	li.setAttribute('id', id);
-	li.setAttribute('onclick', "document.vendorAddress='Skúlagata 28, 101 Reykjavík, Iceland'; moveMapCenter();");
-	li.innerHTML = '<div id="icon">icon goes here</div><div id="details"><div id="name">Vendor Name<br></div><div id="address">Skúlagata 28, 101 Reykjavík, Iceland<br></div><div id="desc">some details about the vendor some details about the vendor some details about the vendor<br></div></div>';
-	ul.appendChild(li);
+function addResultsToList(data) {
+	//for (i=0; i<data.length; i++) {
+	    //char to add: name, address, short desc, 
+	    var currAddress = "256 Thayer St Providence RI 02906";
 
-	//add a marker
+	//TO DO: JUST APPENDS SAME THING
+	    //add them to the DOM
+		var ul = document.getElementById("results-list");
+		var li = document.createElement('li');
+		var id = Math.random(150000); //id of the client
+		li.setAttribute('id', id);
+		li.setAttribute('onclick', "document.vendorAddress='Skúlagata 28, 101 Reykjavík, Iceland'; moveMapCenter();");
+		li.innerHTML = '<div id="icon">icon goes here</div><div id="details"><div id="name">Vendor Name<br></div><div id="address">Skúlagata 28, 101 Reykjavík, Iceland<br></div><div id="desc">some details about the vendor some details about the vendor some details about the vendor<br></div></div>';
+		ul.appendChild(li);
+		addMarker(currAddress);
+	//}
+}
+
+function addMarker(currAddress) {
 	geocoder.geocode( { 'address': currAddress}, function(results, status) {
 		if (status == google.maps.GeocoderStatus.OK) {
 			var marker = new google.maps.Marker({
@@ -64,27 +111,7 @@ function returnResults() {
 	});
 }
 
-//input: results of a search
-//output: places markers on the map corresponding to the coordinates of each result
-/*
-function placeMarker(searchResults) {
-	for (int i=0; i=len(searchResults); i++) {
-		var point = convertToCoord(searchResults[i].address);
-		_map.setCenter(point);
-		var marker = new google.maps.Marker({
-			'map' : _map,
-			'position' : point
-		});
-	}
-}
-*/
 
-function selectVendor(vendor) {
-	alert("selecting a vendor!");
-	//need to make fields for vendors
-	//var vendorID = vendor.ID
-	//window.location.assign("./vendor/"+vendorID);
-}
 
 //TO DO~~~~~~~~~~~~~~~~~~~~~~~~~
 function validateForm() {
@@ -101,3 +128,7 @@ function validateForm() {
             }
         });
 }
+
+
+
+
