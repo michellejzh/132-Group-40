5c2e99539335422687b13b9c78b27e2da7c9c4b9
michellejzh <michelle_zheng@brown.edu>
2014-04-08 12:22:13 -0400

    basic s server, templates, static public set up

--- server.js
+++ server.js
@@ -3,11 +3,41 @@ var app = express();
 app.use(express.json());
 app.use(express.urlencoded());
 
-//chatroom DB
+
 var anyDB = require('any-db');
 var conn = anyDB.createConnection('sqlite3://Database/vendors.db');
 
-//==============SEARCH PAGE==============//
+//using HTML templates
+var engines = require('consolidate');
+app.engine('html', engines.hogan); 
+app.set('views', './' + '/templates');
+
+//finding static local files
+app.use('/public', express.static(__dirname + '/public'));
+
+app.get('/search.json', function(request, response){
+    var messages = [];
+    var q = conn.query('SELECT * FROM messages WHERE roomID=$1', [request.params.roomID]);
+    //should I be putting things in a list here?
+    //problem: it stops working because it doesn't like rows
+    q.on('row', function(row){
+        var nickname = row.nickname;
+        var message = row.message;
+        messages.push({nickname: nickname, message: message});
+    });
+    q.on('end', function(){
+        response.json(messages);
+    });
+});
+
+
+
+
+
+
+
+
+
 app.get('/othertest', function(request, response){
     console.log('- Request received:', request.method, request.url);
     //response.render('search-results.html');
@@ -42,12 +72,18 @@ app.get('/test.json', function(request, response){
     });
 });
 
-
 //==============RESULTS PAGE==============//
-
-
+app.get('/search-results', function(request, response){
+    response.render('searchResults.html');
+});
 
 //============VENDOR LIST PAGE============//
 
 
 //============VENDOR EDIT PAGE============//
+
+
+//==============SEARCH PAGE==============//
+app.get('*', function(request, response){
+    response.render('search.html');
+});
