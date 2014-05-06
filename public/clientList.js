c66b3117c609024169ba080fcde358bbff89990e
ww15 <ww15@cs.brown.edu>
2014-05-06 00:21:47 -0400

    allowed user to query full state names for list of clients

--- public/clientList.js
+++ public/clientList.js
@@ -1,28 +1,41 @@
+/*
+Gets parameters from the URL
+*/
 function getParam(name){
    if(name=(new RegExp('[?&]'+encodeURIComponent(name)+'=([^&]*)')).exec(location.search))
       return decodeURIComponent(name[1]);
 }
 
+/*
+Mapping of states to their abbreviations. Data stored as strings.
+*/
 var stateAbbr = {"alabama":"AL","alaska":"AK","arizona":"AZ","arkansas":"AR","california":"CA","colorado":"CO","connecticut":"CT","delaware":"DC","district of columbia":"DE","florida":"FL","georgia":"GA","hawaii":"HI","idaho":"ID","illinois":"IL","indiana":"IN","iowa":"IA","kansas":"KS","kentucky":"KY","louisiana":"LA","maine":"ME","maryland":"MD","massachusetts":"MA","michigan":"MI","minnesota":"MN","mississippi":"MS","missouri":"MO","montana":"MT","nebraska":"NE","nevada":"NV","new hampshire":"NH","new jersey":"NJ","new mexico":"NM","new york":"NY","north carolina":"NC","north dakota":"ND","ohio":"OH","oklahoma":"OK","oregon":"OR","pennsylvania":"PA","rhode island":"RI","south carolina":"SC","south dakota":"SD","tennessee":"TN","texas":"TX","utah":"UT","vermont":"VT","virginia":"VA","washington":"WA","west virginia":"WV","wisconsin":"WI","wyoming":"WY"};
 
+// id of the form containing a list of the vendors
+var vendorsID = "vendorList";
 
+// runs when document is ready
+$(document).ready(function(){
+	renderVendorList(partner_data);
+});
+
+/*
+Gets the name parameter from the url
+*/
 function getNameFromURL(){
 	var nameFilter = getParam('name');
 	return nameFilter.replace(new RegExp("\\+", 'g'), " ")
 }
 
+/*
+Gets the state parameter from the url
+*/
 function getStateFromURL(){
 	var stateFilter = getParam('state');
 	return stateFilter.replace(new RegExp("\\+", 'g'), " ")
 }
 
-// id of the form containing a list of the vendors
-var vendorsID = "vendorList";
-
-$(document).ready(function(){
-	renderVendorList(partner_data);
-});
-
+// url to visit vendor profiles
 var newURL = window.location.pathname+"../../clientProfile.html";
 
 /*
